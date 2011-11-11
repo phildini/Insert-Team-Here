678a44cb6245c0bf3ce3a6adf743a4e1255d4dfb
Philip James <pjj@philipjohnjames.com>
2011-11-10 17:27:18 -0800

    Made the templates better looking, added some teams as test data.

--- django/InsertTeamHere/teams/views.py
+++ django/InsertTeamHere/teams/views.py
@@ -1,7 +1,7 @@
 from django.template import Context, loader
 from django.shortcuts import render_to_response
 from teams.models import Team
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 def index(request):
@@ -9,5 +9,9 @@ def index(request):
 	return render_to_response('teams/index.html', {'latest_team_list': latest_team_list})
 
 def detail(request, team_id):
-	return HttpResponse("You are now looking at the team with TeamID %s." % team_id)
+	try:
+		t = Team.objects.get(pk=team_id)
+	except Team.DoesNotExist:
+		raise Http404
+	return render_to_response('teams/detail.html', {'team': t})
 
