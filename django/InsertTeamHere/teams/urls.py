f8af85b5b452ffb2488f70688c161100ce3097c1
Philip James <pjj@philipjohnjames.com>
2011-11-11 11:47:22 -0800

    Working with editing team detail fields.

--- django/InsertTeamHere/teams/urls.py
+++ django/InsertTeamHere/teams/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('teams.views',
 	url(r'^$', 'index'),
 	url(r'^(?P<team_id>\d+)/$', 'detail'),
-	url(r'^(?P<poll_id>\d+)/edit/$', 'edit'),
+	url(r'^(?P<team_id>\d+)/edit/$', 'edit'),
 )
--- django/InsertTeamHere/teams/views.py
+++ django/InsertTeamHere/teams/views.py
@@ -4,7 +4,14 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from teams.models import Team
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django import forms
 
+class TeamEdit(forms.Form):
+	name = forms.CharField(max_length=200)
+	genre = forms.CharField(max_length=100)
+        project_type = forms.CharField(max_length=100)
+        city = forms.CharField(max_length=200)
+        state = forms.CharField(max_length=100)
 
 def index(request):
 	latest_team_list = Team.objects.all().order_by('-creation_date')[:5]
@@ -14,3 +21,11 @@ def detail(request, team_id):
 	t = get_object_or_404(Team, pk=team_id)
 	return render_to_response('teams/detail.html', {'team': t}, context_instance=RequestContext(request))
 
+def edit(request, team_id):
+	if request.method == 'POST':
+		form = TeamEdit(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('index')
+	else:
+		form = TeamEdit()
+	return render_to_response('teams/edit.html', {'form':form},)
