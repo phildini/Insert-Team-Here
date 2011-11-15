54b7a282df09b4b96f651d58dcbaaeecfb055a16
Philip James <pjj@philipjohnjames.com>
2011-11-15 15:12:03 -0800

    Updates after Luca Talk.

--- django/InsertTeamHere/urls.py
+++ django/InsertTeamHere/urls.py
@@ -15,7 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
-    #url(r'^accounts/', include('registration.backends.default.urls')),
+
+    #url(r'^accounts/', include('registration.backends.simple.urls')),
 
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
 
