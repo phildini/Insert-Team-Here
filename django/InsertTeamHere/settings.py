5606e171205ec4413c69f7cacdb21aa859cc6aa7
Kelsey Hawley <khawley@ucsc.edu>
2011-11-27 15:36:16 -0800

    updated file references
    Updated settings.py to have the correct file references for templates
    and db

--- django/InsertTeamHere/settings.py
+++ django/InsertTeamHere/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/phildini/Dropbox/School/Insert-Team-Here/django/InsertTeamHere/ith.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/kelseyhawley/github/Insert-Team-Here/django/InsertTeamHere/ith.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/phildini/Dropbox/School/Insert-Team-Here/templates'
+    '/Users/kelseyhawley/github/Insert-Team-Here/templates'
 )
 
 INSTALLED_APPS = (
