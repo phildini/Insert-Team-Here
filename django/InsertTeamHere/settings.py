27d830f4ec2cca6da36f9a2e65eb4754602ba141
Kelsey Hawley <khawley@ucsc.edu>
2011-11-29 09:39:02 -0800

    updated templates and fixed admin site
    Got admin site up and running which required moving a few files back
    into places and changing a setting in settings.py  (I also updated
    links to my local directory).

--- django/InsertTeamHere/settings.py
+++ django/InsertTeamHere/settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/phildini/Dropbox/School/Insert-Team-Here/media/'
+MEDIA_ROOT = '/Users/kelseyhawley/github/School/Insert-Team-Here/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/phildini/Dropbox/School/Insert-Team-Here/static/'
+STATIC_ROOT = '/Users/kelseyhawley/github/School/Insert-Team-Here/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -65,7 +65,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- django/InsertTeamHere/urls.py
+++ django/InsertTeamHere/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     #url(r'^accounts/', include('registration.backends.simple.urls')),
 
