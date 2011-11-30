250ef677b16365b4ffc52c7fd8b0364e32b295f3
Kelsey Hawley <khawley@ucsc.edu>
2011-11-29 22:22:53 -0800

    Standardized state field
    Modified the team state field to be chosen from drop-down.  Also added
    a draft of the filter system to the team/index.html page

--- django/InsertTeamHere/teams/models.py
+++ django/InsertTeamHere/teams/models.py
@@ -1,12 +1,66 @@
 from django.db import models
 
+STATE_CHOICES=(
+	('', ''),
+	('AL', 'Alabama'),
+	('AK','Alaska'),
+	('AZ', 'Arizona'),
+	('AR', 'Arkansas'),
+	('CA', 'California'),
+	('CO', 'Colorado'),
+	('CT', 'Connecticut'),
+	('DE', 'Delaware'),
+	('FL', 'Florida'),
+	('GA', 'Georgia'),
+	('HI','Hawaii'),
+	('ID', 'Idaho'),
+	('IL', 'Illinois'),
+	('IN', 'Indiana'),
+	('IA', 'Iowa'),
+	('KS', 'Kansas'),
+	('KY', 'Kentucky'),
+	('LA', 'Louisiana'),
+	('ME', 'Maine'),
+	('MD', 'Maryland'),
+	('MA', 'Massachusetts'),
+	('MI', 'Michigan'),
+	('MN', 'Minnesota'),
+	('MS', 'Mississippi'),
+	('MO', 'Missouri'),
+	('MT', 'Montana'),
+	('NE', 'Nebraska'),
+	('NV', 'Nevada'),
+	('NH', 'New Hampshire'),
+	('NJ', 'New Jersey'),
+	('NM', 'New Mexico'),
+	('NY', 'New York'),
+	('NC', 'North Carolina'),
+	('ND', 'North Dakota'),
+	('OH', 'Ohio'),
+	('OK', 'Oklahoma'),
+	('OR', 'Oregon'),
+	('PA', 'Pennsylvania'),
+	('RI', 'Rhode Island'),
+	('SC', 'South Carolina'),
+	('SD', 'South Dakota'),
+	('TN', 'Tennessee'),
+	('TX', 'Texas'),
+	('UT', 'Utah'),
+	('VT', 'Vermont'),
+	('VA', 'Virginia'),
+	('WA', 'Washington'),
+	('WV', 'West Virginia'),
+	('WI', 'Wisconsin'),
+	('WY', 'Wyoming'),
+)
+
 class Team(models.Model):
 	team_name = models.CharField(max_length=200)
 	creation_date = models.DateTimeField('date created')
 	genre = models.CharField(max_length=100)
 	project_type = models.CharField(max_length=100)
 	city = models.CharField(max_length=200)
-	state = models.CharField(max_length=2)
+	state = models.CharField(max_length=1, choices=STATE_CHOICES)
 
 	def __unicode__(self):
 		return self.team_name
--- django/InsertTeamHere/teams/views.py
+++ django/InsertTeamHere/teams/views.py
@@ -7,14 +7,69 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django import forms
 import datetime
 
+STATE_CHOICES=(
+	('', ''),
+	('AL', 'Alabama'),
+	('AK','Alaska'),
+	('AZ', 'Arizona'),
+	('AR', 'Arkansas'),
+	('CA', 'California'),
+	('CO', 'Colorado'),
+	('CT', 'Connecticut'),
+	('DE', 'Delaware'),
+	('FL', 'Florida'),
+	('GA', 'Georgia'),
+	('HI','Hawaii'),
+	('ID', 'Idaho'),
+	('IL', 'Illinois'),
+	('IN', 'Indiana'),
+	('IA', 'Iowa'),
+	('KS', 'Kansas'),
+	('KY', 'Kentucky'),
+	('LA', 'Louisiana'),
+	('ME', 'Maine'),
+	('MD', 'Maryland'),
+	('MA', 'Massachusetts'),
+	('MI', 'Michigan'),
+	('MN', 'Minnesota'),
+	('MS', 'Mississippi'),
+	('MO', 'Missouri'),
+	('MT', 'Montana'),
+	('NE', 'Nebraska'),
+	('NV', 'Nevada'),
+	('NH', 'New Hampshire'),
+	('NJ', 'New Jersey'),
+	('NM', 'New Mexico'),
+	('NY', 'New York'),
+	('NC', 'North Carolina'),
+	('ND', 'North Dakota'),
+	('OH', 'Ohio'),
+	('OK', 'Oklahoma'),
+	('OR', 'Oregon'),
+	('PA', 'Pennsylvania'),
+	('RI', 'Rhode Island'),
+	('SC', 'South Carolina'),
+	('SD', 'South Dakota'),
+	('TN', 'Tennessee'),
+	('TX', 'Texas'),
+	('UT', 'Utah'),
+	('VT', 'Vermont'),
+	('VA', 'Virginia'),
+	('WA', 'Washington'),
+	('WV', 'West Virginia'),
+	('WI', 'Wisconsin'),
+	('WY', 'Wyoming'),
+)
+
 class TeamEdit(forms.Form):
 	name = forms.CharField(max_length=200, required=False)
 	genre = forms.CharField(max_length=100, required=False)
         project_type = forms.CharField(max_length=100, required=False)
         city = forms.CharField(max_length=200, required=False)
-        state = forms.CharField(max_length=2, required=False)
+        state = forms.ChoiceField(choices=STATE_CHOICES)
 
 def index(request):
+	states=STATE_CHOICES
 	latest_team_list = Team.objects.all().order_by('-creation_date')[:100]
 	return render_to_response('teams/index.html', {'latest_team_list': latest_team_list})
 
