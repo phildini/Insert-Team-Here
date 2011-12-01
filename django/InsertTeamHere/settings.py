57992da8214fb1dccc8982997b35e27d3eb70828
Philip James <pjj@philipjohnjames.com>
2011-12-01 11:21:56 -0800

    MOAR Updates.

--- django/InsertTeamHere/settings.py
+++ django/InsertTeamHere/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/phildini/Dropbox/School/Insert-Team-Here/django/InsertTeamHere/ith.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/kelseyhawley/github/Insert-Team-Here/django/InsertTeamHere/ith.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/phildini/Dropbox/School/Insert-Team-Here/media/'
+MEDIA_ROOT = '/Users/kelseyhawley/github/School/Insert-Team-Here/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/phildini/Dropbox/School/Insert-Team-Here/static/'
+STATIC_ROOT = '/Users/kelseyhawley/github/School/Insert-Team-Here/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -65,7 +65,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/phildini/Dropbox/School/Insert-Team-Here/templates'
+    '/Users/kelseyhawley/github/Insert-Team-Here/templates'
 )
 
 INSTALLED_APPS = (
